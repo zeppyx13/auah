95e30bd29df013f9cd56035b5fd049b6cb40a467
Gung Nanda <gungnands@gmail.com>
2025-02-06 17:48:08 +0800

    test 3

--- src/views/users/Dashboard.jsx
+++ src/views/users/Dashboard.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const Dashboard = () => {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1>Dashboard users</h1>
     </div>
   );
 };
